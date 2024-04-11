f0e630c77a26f6fc8e3cf12dbd6078a9fd839f52
MelAna <htc0979504151@gmail.com>
2024-04-11 23:50:00 +0300

    filling the database

--- QA_Auto table.sql
+++ QA_Auto table.sql
@@ -1,25 +1,29 @@
 create schema QA_AUTO;
-create table QA_AUTO.car_brands (
-id int, 
-title varchar (255)
+create table car_brands (
+id int primary key auto_increment, 
+title varchar (255) not null
 );
-create table QA_AUTO.car_models (
-id int, 
+create table car_models (
+id int primary key auto_increment, 
 carBrandId int, 
-title varchar (255)
+foreign key (carBrandId) references car_brands (id),
+title varchar (255) not null
 );
-create table QA_AUTO.users (
-id int, 
-firstName varchar (255), 
-lastName varchar (255), 
-email varchar (255), 
-password varchar (255)
+create table users (
+id int primary key auto_increment, 
+firstName varchar (255) not null, 
+lastName varchar (255) not null, 
+email varchar (255) not null, 
+password varchar (255) not null
 );
-create table QA_AUTO.cars (
-id int, 
+create table cars (
+id int primary key auto_increment, 
 userId int, 
+foreign key (userId) references users (id),
 carBrandId int, 
+foreign key (carBrandId) references car_brands (id),
 carModelId int, 
-mileage float, 
+foreign key (carModelId) references car_models (id),
+mileage float , 
 initialMilleage float
 );
